a65c9fa60b8da49055d58c4cabf641fb0183af6f
princek <princesamuelpks@gmail>
2025-09-20 16:22:34 +0000

    Update urls.py

--- sitemonitorcore/urls.py
+++ sitemonitorcore/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for sitemonitorcore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from sitemonitor.views import register, dashboard
+from sitemonitor.routing import urlpatterns as api_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/register/', register, name='register'),
+    path('api/auth/', include('knox.urls')),
+    path('api/dashboard/', dashboard, name='dashboard'),
+    path('api/', include((api_router, 'sitemonitor'), namespace='sitemonitor')),  # routers
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema')), 
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema')), 
 ]
